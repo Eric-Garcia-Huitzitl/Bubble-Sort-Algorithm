32fb2fe10c2d895073cd0da1a4b6dcc0f4c0632d
Eric-Garcia-Huitzitl <cireckfccbuap@gmail.com>
2019-07-14 12:20:14 -0500

    two version

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,7 +1,32 @@
-
 #include <iostream> 
 using namespace std; 
+
+typedef int Type;
+
+
+
 int main(){
-cout <<"Hola"<<endl;
+
+  Type array[5]={3,9,4,5,1};
+  int i = 0,j = 0,aux = 0; 
+  for(i = 0; i<5; i++){
+    cout <<"hola"<<endl; 
+    for(j = i+1; j<5; j++){
+      if(array[i]>array[j]){
+	cout<<array[i]<<" es mayor que "<<array[j]<<endl; 
+	aux=array[j];
+	array[j]=array[i];
+	array[i]=aux; 
+      }else{} 
+    }
+
+  }
+  
+
+  for(int i = 0; i < 5; i++){
+    cout <<array[i]<<" --"; 
+  }
+  
+cout << "array to 100 elements"<<endl; 
 return 0;
 }
